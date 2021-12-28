72da4339a293ace7c36097a49f8a6ba5b2248b10
brain <shahriarmehedi95@gmail.com>
2021-12-28 15:45:25 +0600

    card add

--- src/components/home/Profile.jsx
+++ src/components/home/Profile.jsx
@@ -1,5 +1,5 @@
 import React, { Component, Fragment } from 'react'
-import {Container,Row,Col,Card} from 'react-bootstrap'
+import {Container,Row,Col,Card, Button} from 'react-bootstrap'
 import axios from 'axios'
 import AppUrl from '../../api/AppUrl';
 
@@ -21,12 +21,29 @@ class Profile extends Component {
         const ShowData=this.state.allData;
         const View=ShowData.map((ShowData,i)=>{
             return (
-                <Col key={i.toString()} className="p-0" xl={3} lg={3} md={3} sm={6} xs={6}>
+                <Col key={i.toString()} className="p-1" xl={3} lg={3} md={3} sm={6} xs={6}>
                 <Card className="image-box card w-100">
-                <img alt='' className="center w-75" src={ShowData.image} />   
+
+
+                {
+                    (ShowData.employee).map((Imagelist,i)=>{
+                        return  <img alt='' className="center w-75" src={Imagelist.image} />
+
+                    })
+                }
+                  
                 <Card.Body> 
                 <p className="product-name-on-card">Name :  {ShowData.name}</p>
-                <p className="product-price-on-card">Designation : {ShowData.designation}</p>     
+
+                {
+                    (ShowData.employee).map((Imagelist,i)=>{
+                        return  <p className="product-price-on-card">Designation : {Imagelist.designation}</p>
+                        
+                    })
+                }
+                <Button>See Details</Button>
+
+                     
                 </Card.Body>
                 </Card>          
                 </Col>
@@ -35,7 +52,8 @@ class Profile extends Component {
         return (
             <Fragment>
             <Container className="text-center" fluid={true}>
-   <div className="section-title text-center mb-55"><h2>EMPLOYEES</h2>
+   <div className="section-title text-center mb-55">
+       <h2>EMPLOYEES</h2>
    <p>Global Skills Development Agency</p>
    </div>
 
